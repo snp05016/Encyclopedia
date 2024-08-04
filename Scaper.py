216defc61c41c245fb9d781934ea932e41dd8640
snp050106 <snp050106@gmail.com>
2024-08-04 19:27:33 +0400

    Getting basic info

--- Scaper.py
+++ Scaper.py
@@ -1,9 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 
-toi = requests.get('https://en.wikipedia.org/wiki/Shubman_Gill  ').text
-
+toi = requests.get('https://en.wikipedia.org/wiki/Shikhar_Dhawan').text
 soup = BeautifulSoup(toi, 'lxml')
 container = soup.find('div',{'class':'mw-content-ltr mw-parser-output'})
-para = container.find_all('p')[2].text
-print(para)
+para = container.find_all('p')
+
+plist = []
+
+for p in range(0,len(para)-1):
+    plist.append(p) if 'Shikhar Dhawan' in para[p].text else None
+
+# Print the first paragraph with 'dog' in it
+requiredtext = para[plist[0]].text
+print(requiredtext)
+
